20270fb7b2c69c98015c777a6f5a6e52ff5faf24
lerenardprudent <daniel.margulius@gmail.com>
2014-01-10 12:01:11 -0500

    Créé un raccourci et un exécutable.

--- so.bat
+++ so.bat
@@ -1,6 +1,14 @@
-@echo off
 set http_port=6767
-start %~dp0php -S localhost:%http_port% -t "%~dp0"
+set php_process=php-hl.exe
+set temp_outfile1=%temp%\search.log
+set temp_outfile2=%temp%\found.log
 start http://localhost:%http_port%/index.html
+tasklist /FI "IMAGENAME eq %php_process%" /FO CSV > %temp_outfile1%
+findstr %php_process% %temp_outfile1% > %temp_outfile2%
+FOR /F %%A IN (%temp_outfile2%) DO IF %%~zA EQU 0 GOTO end
+%~dp0%php_process% -S localhost:%http_port% -t "%~dp0"
+:end
+REM del %temp_outfile1%
+REM del %temp_outfile2%
 REM start http://localhost/hors/
 REM wscript C:\Users\LAP-PRO\Documents\GitHub\HorsLigne\invisible.vbs C:\Users\LAP-PRO\Documents\GitHub\HorsLigne\so.bat
